cbad9d73dcfcbc81ed3f88198df4d67cd76b965a
ThatoR <thatoramphore.info@gmail.com>
2025-11-24 21:13:46 +0200

    feat(MovieCard): add a Card component

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+
+import './App.css'
+import MovieCard from './components/MovieCard'
+
+function App() {
+  return (
+    <>
+      <MovieCard movie={{ title: "TT: Reloaded", release_date: "2025" }} />
+    </>
+  )
+}
+
+export default App
